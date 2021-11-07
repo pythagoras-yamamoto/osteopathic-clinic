06877378727bfc83b28d6175988bce900866f684
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-07 23:14:15 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <h1>aaaa</h1>
       <h2>Start editing to see some magic happen!</h2>
       <h3>ちんこ</h3>
-      <h1>うんこ</h1>
+      <h1>うんこここ</h1>
       <h1>うんこ大魔神</h1>
     </div>
   );

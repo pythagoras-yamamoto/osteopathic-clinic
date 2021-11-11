a5215e77f0a4d7bd60542d0f42243911b2353cc1
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-11 20:37:30 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
+import { HeaderMenuButton } from "./components/HeaderMenuButton";
+import { HeaderMenu } from "./components/HeaderMenu";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h1>aaaaiiii</h1>
-      <h1>aaaa</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <h3>ちんこ</h3>
-      <h1>うんこここ</h1>
-      <h1>うんこここ</h1>
-      <h1>うんこここ</h1>
-      <h1>うんこ大魔神</h1>
+      <HeaderMenuButton>test</HeaderMenuButton>
+      <HeaderMenu></HeaderMenu>
     </div>
   );
 }
--- src/components/HeaderMenuButton.jsx
+++ src/components/HeaderMenuButton.jsx
@@ -1,6 +1,24 @@
 import styled from "styled-components";
 
 //ヘッダー部分のメニューボタンのコンポーネント
-export const HeaderArea = () => {
-  return <></>;
+export const HeaderMenuButton = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <SButton>
+        <button>{children}</button>
+      </SButton>
+      <button>{children}</button>
+    </>
+  );
 };
+
+const SButton = styled.button`
+  padding: 6px 12px;
+  border: none;
+  border-radius: 5px;
+  &:hover {
+    background: blue;
+    color: white;
+  }
+`;

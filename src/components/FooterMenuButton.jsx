91b6494afd2df78acd325d13f79c66a3ce6547c4
yochan0103 <68014172+yochan0103@users.noreply.github.com>
2021-11-11 22:52:06 +0900

    initial commit

--- src/components/FooterMenuButton.jsx
+++ src/components/FooterMenuButton.jsx
@@ -1,6 +1,22 @@
 import styled from "styled-components";
+import React from "react";
 
 //フッター部分のメニューボタンのコンポーネント
-export const HeaderArea = () => {
-  return <></>;
+export const FooterMenuButton = () => {
+  return (
+    <div className="LinkButton">
+      <a className="home" href="./pages/HomePage.jsx">
+        ホーム
+      </a>
+      <a className="about" href="./pages/AboutUsPage.jsx">
+        当院について
+      </a>
+      <a className="reservation" href="./pages/ReservationPage.jsx">
+        料金・予約
+      </a>
+      <a className="inquery" href="./pages/InqueryPage.jsx">
+        お問合せ
+      </a>
+    </div>
+  );
 };

98225a80f323d072288b543e5bce8d0f0a538764
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-13 11:47:02 +0900

    header menu 修正 flexbox適用し横並びに

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <HeaderMenuButton>test</HeaderMenuButton>
       <HeaderMenu></HeaderMenu>
     </div>
   );

d0466cf08cb4731b5ecb129061f4fb9ee2a4b028
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-15 21:54:22 +0900

    initial commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,41 @@
 import styled from "styled-components";
 
-import { HeaderMenu } from "./HeaderMenu";
-import { FooterMenu } from "./FooterMenu";
+import { HeaderMenu } from "../components/HeaderMenu.jsx";
+import { FooterMenu } from "../components/FooterMenu.jsx";
+import Image from "../clinic-image.jpeg";
 
-export const BaseLayout = () => {
+export const HomePage = () => {
   return (
     <>
       <HeaderMenu />
+      <SClinicImage>
+        <STitleOnClinicImage>根津整体院</STitleOnClinicImage>
+        <img src={Image} alt="" height={900} width={1440} />
+      </SClinicImage>
       <FooterMenu />
     </>
   );
 };
+
+const SClinicImage = styled.div`
+  position: relative;
+`;
+
+const STitleOnClinicImage = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  font-size: 50px;
+  color: white;
+  font-weight: bold;
+  margin: 0;
+  padding: 0;
+`;
+/*
+const STextOnClinicImage = styled.p`
+  position: absolute;
+  top: 60%;
+  text-align: center;
+  font-size: 20px;
+  color: white;
+`;*/

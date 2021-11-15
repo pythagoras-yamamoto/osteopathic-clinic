cd4e7aebfccf78552791a1864c69ea3e09a96385
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-15 14:46:43 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { HeaderMenu } from "./components/HeaderMenu";
 
 import "./styles.css";
 import { FooterMenu } from "./components/FooterMenu";
+import { BaseLayout } from "./components/BaseLayout";
 
 export default function App() {
   return (
     <div className="App">
-      <HeaderMenu></HeaderMenu>
-      <FooterMenu />
+      <BaseLayout />
     </div>
   );
 }

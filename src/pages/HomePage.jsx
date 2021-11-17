a78d7071e0ec14d0f61734aab57ce9c2ec9e7227
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-17 14:30:44 +0900

    initial commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,40 +8,41 @@ export const HomePage = () => {
   return (
     <>
       <HeaderMenu />
-      <SContainer>
-        <STitleOnClinicImage>根津整体院</STitleOnClinicImage>
+      <SImageWraper>
+        <STitleOnImage>根津整体院</STitleOnImage>
+        <STextOnImage>
+          女性施術者による女性専用サロンです。それぞれのお身体,
+          体調に合わせたオーダーメイドの鍼灸施術を行います
+        </STextOnImage>
         <SImage src={Image} alt="" />
-      </SContainer>
+      </SImageWraper>
       <FooterMenu />
     </>
   );
 };
 
-const SContainer = styled.div`
+const SImageWraper = styled.div`
   position: relative;
 `;
 
-const STitleOnClinicImage = styled.p`
+const STitleOnImage = styled.p`
   position: absolute;
   top: 30%;
-  left: 50%;
   font-size: 80px;
-  color: white;
   font-weight: bold;
-  margin: 0;
-  padding: 0;
+  color: white;
+  margin-left: 50px;
+`;
+
+const STextOnImage = styled(STitleOnImage)`
+  text-align: left;
+  font-weight: normal;
+  font-size: 20px;
+  width: 530px;
+  margin-top: 180px;
 `;
 
 const SImage = styled.img`
   width: 100vw;
   height: auto;
 `;
-
-/*
-const STextOnClinicImage = styled.p`
-  position: absolute;
-  top: 60%;
-  text-align: center;
-  font-size: 20px;
-  color: white;
-`;*/

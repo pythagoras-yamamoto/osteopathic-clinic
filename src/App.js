ac26267bdb1edee314a03d06984f0a5173b226c9
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-19 16:44:04 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./styles.css";
 //import { HeaderMenu } from "./components/HeaderMenu";
 //import { FooterMenu } from "./components/FooterMenu";
-import { HomePage } from "./pages/HomePage";
+//import { HomePage } from "./pages/HomePage";
+import { Router } from "./router/Router";
+import { BrowserRouter } from "react-router-dom";
+
 // import Image from "./clinic-image.jpeg";
 
 export default function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 }

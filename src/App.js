0f03172419573cf0fd6cf6b94960ecb26d8245cd
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-19 16:52:12 +0900

    app.js 修正

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import { Router } from "./router/Router";
 import { BrowserRouter } from "react-router-dom";
 
 import "./styles.css";
-//import { HeaderMenu } from "./components/HeaderMenu";
-//import { FooterMenu } from "./components/FooterMenu";
-//import { HomePage } from "./pages/HomePage";
 
 export default function App() {
   return (

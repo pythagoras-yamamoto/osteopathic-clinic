e7a7f0a47dc53d310d0510b028d71970c8e1c372
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-19 16:48:31 +0900

    Appのimport箇所炉ファクタリング

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import { Router } from "./router/Router";
+import { BrowserRouter } from "react-router-dom";
+
 import "./styles.css";
 //import { HeaderMenu } from "./components/HeaderMenu";
 //import { FooterMenu } from "./components/FooterMenu";
 //import { HomePage } from "./pages/HomePage";
-import { Router } from "./router/Router";
-import { BrowserRouter } from "react-router-dom";
-
-// import Image from "./clinic-image.jpeg";
 
 export default function App() {
   return (

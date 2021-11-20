70aa2e1dab40d5543b172ba01cd2886e1b2fd61c
yochan0103 <68014172+yochan0103@users.noreply.github.com>
2021-11-20 15:09:05 +0900

    initial commit

--- src/components/BaseLayout.jsx
+++ src/components/BaseLayout.jsx
@@ -3,11 +3,13 @@ import styled from "styled-components";
 
 import { HeaderMenu } from "./HeaderMenu";
 import { FooterMenu } from "./FooterMenu";
+import { Container } from "./Container";
 
 export const BaseLayout = () => {
   return (
     <>
       <HeaderMenu />
+      <Container />
       <FooterMenu />
     </>
   );

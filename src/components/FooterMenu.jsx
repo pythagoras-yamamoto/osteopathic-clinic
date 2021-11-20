0a198a06811262d290629035f578b03c7b5ab096
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-20 22:06:24 +0900

    footer 配置変更

--- src/components/FooterMenu.jsx
+++ src/components/FooterMenu.jsx
@@ -28,53 +28,23 @@ export const FooterMenu = () => {
             </Link>
           </li>
         </SFooterMenu>
-        <STelinfo>
-          <p className="info-child">電話番号</p>
-        </STelinfo>
-        <STelinfo>
-          <p className="info-child">住所</p>
-        </STelinfo>
+        <SInfoWrapper>
+          <STelinfo>電話番号</STelinfo>
+          <SAdressinfo>住所</SAdressinfo>
+        </SInfoWrapper>
         <SCopyright>© 2021 Netsu Osteopathic Clinic</SCopyright>
       </div>
     </SFooter>
   );
 };
 
-// const SFooterMenu = styled.div`
-//   width: 100%;
-//   height: 120px;
-//   background-color: #d3d3d3;
-//   /* border-top: solid 5px #ffb6c1; */
-//   /* border-bottom: solid 5px #ffb6c1; */
-//   opacity: 0.8;
-// `;
-
-// const SHosName = styled.p`
-//   text-align: center;
-//   position: center;
-// `;
-
 const SFooter = styled.div`
   width: 100%;
   background-color: #d3d3d3;
+  position: relative;
 
-  .tel-info {
-    display: flex;
-    justify-content: flex-end;
-    position: relative;
-    top: -40px;
-  }
-  .access-info {
-    display: flex;
-    justify-content: flex-end;
-    position: relative;
-    top: -30px;
-  }
   @media screen and (max-width: 780px) {
   }
-  /* position: relative; */
-  /* top: 20px; */
-  /* left: 40%; */
 `;
 
 const SFooterMenu = styled.ul`
@@ -91,21 +61,26 @@ const SFooterMenu = styled.ul`
   }
 `;
 
-const STelinfo = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  position: relative;
-  top: 0px;
-  margin: 5px 10px 5px 0;
-
-  .info-child {
-    display: inline-block;
-    border-radius: 8px;
-    background-color: white;
-    width: 100px;
-    height: 30px;
-  }
+const SInfoWrapper = styled.div`
+  position: absolute;
+  z-index: 100;
+  top: 0;
+  right: 50px;
+  /* display: inline-block; */
+  /* background-color: white; */
 `;
+
+const STelinfo = styled.p`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  background-color: white;
+  margin: 5px;
+  padding: 10px;
+  border-radius: 8px;
+`;
+
+const SAdressinfo = styled(STelinfo)``;
 
 const SCopyright = styled.div`
   display: flex;

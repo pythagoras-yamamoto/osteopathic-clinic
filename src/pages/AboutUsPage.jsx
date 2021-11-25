a8961d9e2a06db2e4c2338c54140a4378668d74c
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-26 01:50:58 +0900

    各ページ追加

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,13 +1,42 @@
-//import styled from "styled-components";
+import styled from "styled-components";
 
-import { HeaderMenu } from "../components/HeaderMenu";
-import { FooterMenu } from "../components/FooterMenu";
+import { HeaderMenu } from "../components/HeaderMenu.jsx";
+import { ContainerA } from "../components/ContainerA";
+import { FooterMenu } from "../components/FooterMenu.jsx";
+import Image from "../../public/clinic-image.jpeg";
 
 export const AboutUsPage = () => {
   return (
     <>
       <HeaderMenu />
+      <SImageWraper>
+        <SImage src={Image} alt="" />
+      </SImageWraper>
+      <ContainerA
+        title={"タイトル"}
+        text={
+          "こんにちは\n根津です。こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは"
+        }
+        image={Image}
+        shadowColor={"#de8787"}
+      />
+      <ContainerA
+        title={"ご案内"}
+        text={"こんにちは\n根津です。こんにちはこんにちはこんにちはこんにちは"}
+        image={Image}
+        widthRatio={50}
+      />
       <FooterMenu />
     </>
   );
 };
+
+const SImageWraper = styled.div`
+  position: relative;
+`;
+
+const SImage = styled.img`
+  object-fit: cover;
+  width: 100vw;
+  height: 300px;
+`;

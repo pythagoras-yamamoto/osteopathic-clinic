3fea9f62f582176426437172bcc70107be068034
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-26 19:55:39 +0900

    delete margin made by slick slickの余白問題を解消

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -5,28 +5,39 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import Img from "../../public/clinic-image.jpeg";
+//import { Autoplay } from "swiper";
 
 export const ImageSlider = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 1000,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplaySpeed: 6000,
+    autoplay: true
   };
 
   return (
-    <Slider {...settings}>
-      <SImage src={Img} alt="" />
-      <SImage src={Img} alt="" />
-      <SImage src={Img} alt="" />
-    </Slider>
+    <ImgWrapper>
+      <Slider {...settings}>
+        <SImage src={Img} alt="" />
+        <SImage src={Img} alt="" />
+        <SImage src={Img} alt="" />
+      </Slider>
+    </ImgWrapper>
   );
 };
 
 const SImage = styled.img`
-  width: 100vw;
+  width: 100vh;
   height: auto;
+  box-shadow: 0px 5px 0 rgb(0 0 0 / 15%);
+`;
+
+// slickの仕様で右側に余白ができる。それを解消するために使用したスタイリング。
+const ImgWrapper = styled.div`
+  overflow: hidden;
 `;
 
 // import "swiper/swiper.min.css";

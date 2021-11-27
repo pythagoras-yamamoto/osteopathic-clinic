93a9dca4a5b9ef9463c3eed70bcdf7ae8bd26178
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-11-27 13:10:15 +0900

    insert image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,9 @@ import { ContainerA } from "../components/ContainerA";
 import { FooterMenu } from "../components/FooterMenu.jsx";
 import { ImageSlider } from "../components/ImageSlider";
 import Map from "../components/Map";
+
 import Image from "../../public/clinic-image.jpeg";
+
 //import Img from "../../public/medical_harikyu_woman.png";
 
 export const HomePage = () => {

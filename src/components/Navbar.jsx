88d4a8e1567cf2c5632af51cd441cd1daddeea4a
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-12-06 22:33:11 +0900

    humberger  プロトタイプ完成

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,41 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-  const [showLinks, setShowLinks] = useState(false);
+  const [extendNavbar, setExtendNavbar] = useState(false);
 
   return (
     <SNavWrapper>
       <SNavLogo>
-        <Link to="/" className="nav-link-element">
+        <Link to="/">
           {/* TODO : INSERT LOGO IAMGE */}
           LOGO
         </Link>
       </SNavLogo>
-      <SNavMenu id={showLinks ? "hidden" : ""}>
+      <SNavMenu>
         <SNavLink to="/">Home</SNavLink>
         <SNavLink to="/AboutUsPage">About Me</SNavLink>
         <SNavLink to="/ResearvationPage">Service & Menu</SNavLink>
         <SNavLink to="/ContactPage">Contact</SNavLink>
-        <OpenLinksButton>&#8801;</OpenLinksButton>
+        <OpenLinksButton
+          onClick={() => {
+            setExtendNavbar((curr) => !curr);
+          }}
+        >
+          {extendNavbar ? <>&#10005;</> : <> &#8801;</>}
+
+          {extendNavbar && (
+            <NavbarExtendedContainer>
+              <NavbarLinkExtended to="/">Home</NavbarLinkExtended>
+              <NavbarLinkExtended to="/AboutUsPage">
+                About Me
+              </NavbarLinkExtended>
+              <NavbarLinkExtended to="/ResearvationPage">
+                Service & Menu
+              </NavbarLinkExtended>
+              <NavbarLinkExtended to="/ContactPage">Contact</NavbarLinkExtended>
+            </NavbarExtendedContainer>
+          )}
+        </OpenLinksButton>
       </SNavMenu>
     </SNavWrapper>
   );
@@ -46,7 +65,6 @@ const SNavLogo = styled.h1`
 const SNavMenu = styled.ul`
   display: grid;
   grid-template-columns: repeat(5, auto);
-  grid-gap: 10px;
   text-align: center;
   justify-content: end;
   width: 70vw;
@@ -57,7 +75,7 @@ const SNavLink = styled(Link)`
   transition: 0.2s;
   padding-left: 2em;
   font-weight: bold;
-  font-size: 16px;
+  font-size: 15px;
   color: black;
   text-decoration: none;
 
@@ -65,21 +83,44 @@ const SNavLink = styled(Link)`
     opacity: 0.5;
   }
 
-  /* @media screen and (max-width: 780px) {
+  @media screen and (max-width: 700px) {
     display: none;
-  } */
+  }
 `;
 
 const OpenLinksButton = styled.button`
-  width: 70px;
   height: 50px;
   background: none;
   border: none;
   color: black;
-  font-size: 45px;
+  font-size: 40px;
   cursor: pointer;
 
   @media (min-width: 700px) {
     display: none;
   }
+`;
+
+//🍔作り中
+
+const NavbarExtendedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  width: 200px;
+  box-shadow: 0px 2px 10px rgb(0 0 0 / 10%);
+
+  @media (min-width: 700px) {
+    display: none;
+  }
+`;
+
+const NavbarLinkExtended = styled(Link)`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: none;
+  margin: 10px;
 `;

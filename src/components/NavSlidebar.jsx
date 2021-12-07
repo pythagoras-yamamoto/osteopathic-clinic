69baa7b00bb6c140e66f0421da5a6be7e07d565b
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-12-07 12:24:11 +0900

    initial commit

---
+++ src/components/NavSlidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { NavSliderContainer, Icon, CloseIcon } from "./NavSlidebarElements";
+
+export const NavSlidebar = () => {
+  return (
+    <NavSliderContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+    </NavSliderContainer>
+  );
+};

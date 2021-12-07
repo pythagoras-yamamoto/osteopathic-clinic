e0c87f74c097474ac26bd060af13620537c2e4dd
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-12-07 11:44:44 +0900

    Navbarを4つのコンポーネントに分割

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+
+import {
+  SNavWrapper,
+  SNavLogo,
+  SNavMenu,
+  SNavbarExtendedContainer,
+  SOpenLinksButton,
+  SNavLink,
+  SNavbarLinkExtended
+} from "./NavbarElements";
 
 export const Navbar = () => {
   const [extendNavbar, setExtendNavbar] = useState(false);
@@ -18,109 +28,31 @@ export const Navbar = () => {
         <SNavLink to="/AboutUsPage">当院について</SNavLink>
         <SNavLink to="/ResearvationPage">サービス・料金</SNavLink>
         <SNavLink to="/ContactPage">お問い合わせ</SNavLink>
-        <OpenLinksButton
+
+        <SOpenLinksButton
           onClick={() => {
             setExtendNavbar((curr) => !curr);
           }}
         >
-          {extendNavbar ? <>&#10005;</> : <> &#8801;</>}
+          {/* {extendNavbar ? <>&#10005;</> : <> &#8801;</>} */}
+          {extendNavbar ? <>&#10005;</> : <FaBars />}
 
           {extendNavbar && (
-            <NavbarExtendedContainer>
-              <NavbarLinkExtended to="/">Home</NavbarLinkExtended>
-              <NavbarLinkExtended to="/AboutUsPage">
+            <SNavbarExtendedContainer>
+              <SNavbarLinkExtended to="/">Home</SNavbarLinkExtended>
+              <SNavbarLinkExtended to="/AboutUsPage">
                 About Me
-              </NavbarLinkExtended>
-              <NavbarLinkExtended to="/ResearvationPage">
+              </SNavbarLinkExtended>
+              <SNavbarLinkExtended to="/ResearvationPage">
                 Service & Menu
-              </NavbarLinkExtended>
-              <NavbarLinkExtended to="/ContactPage">Contact</NavbarLinkExtended>
-            </NavbarExtendedContainer>
+              </SNavbarLinkExtended>
+              <SNavbarLinkExtended to="/ContactPage">
+                Contact
+              </SNavbarLinkExtended>
+            </SNavbarExtendedContainer>
           )}
-        </OpenLinksButton>
+        </SOpenLinksButton>
       </SNavMenu>
     </SNavWrapper>
   );
 };
-
-const SNavWrapper = styled.nav`
-  background-color: white;
-  height: 80px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 200;
-  width: 100vw;
-  position: fixed;
-  opacity: 0.9;
-  box-shadow: 0px 2px 10px rgb(0 0 0 / 10%);
-`;
-
-const SNavLogo = styled.h1`
-  color: black;
-  justify-self: start;
-  cursor: pointer;
-`;
-
-const SNavMenu = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(5, auto);
-  text-align: center;
-  justify-content: end;
-  width: 70vw;
-  list-style: none;
-`;
-
-const SNavLink = styled(Link)`
-  transition: 0.2s;
-  padding-left: 2em;
-  font-weight: bold;
-  font-size: 15px;
-  color: black;
-  text-decoration: none;
-
-  &:hover {
-    opacity: 0.5;
-  }
-
-  @media screen and (max-width: 700px) {
-    display: none;
-  }
-`;
-
-const OpenLinksButton = styled.button`
-  height: 50px;
-  background: none;
-  border: none;
-  color: black;
-  font-size: 40px;
-  cursor: pointer;
-
-  @media (min-width: 700px) {
-    display: none;
-  }
-`;
-
-//🍔作り中
-
-const NavbarExtendedContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: white;
-  width: 200px;
-  box-shadow: 0px 2px 10px rgb(0 0 0 / 10%);
-
-  @media (min-width: 700px) {
-    display: none;
-  }
-`;
-
-const NavbarLinkExtended = styled(Link)`
-  color: black;
-  font-size: 16px;
-  font-weight: bold;
-  font-family: Arial, Helvetica, sans-serif;
-  text-decoration: none;
-  margin: 10px;
-`;

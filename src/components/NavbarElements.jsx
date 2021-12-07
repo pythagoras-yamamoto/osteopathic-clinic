4d969649eb2a7a625afd6e865f8d5d07b492eed6
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-12-07 11:44:44 +0900

    Navbarを4つのコンポーネントに分割

---
+++ src/components/NavbarElements.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const SNavWrapper = styled.nav`
+  background-color: white;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 200;
+  width: 100vw;
+  position: fixed;
+  opacity: 0.9;
+  box-shadow: 0px 2px 10px rgb(0 0 0 / 10%);
+`;
+
+export const SNavLogo = styled.h1`
+  color: black;
+  justify-self: start;
+  cursor: pointer;
+`;
+
+export const SNavMenu = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(5, auto);
+  text-align: center;
+  justify-content: end;
+  width: 70vw;
+  list-style: none;
+`;
+
+export const SNavLink = styled(Link)`
+  transition: 0.2s;
+  padding-left: 2em;
+  font-weight: bold;
+  font-size: 15px;
+  color: black;
+  text-decoration: none;
+
+  &:hover {
+    opacity: 0.5;
+  }
+
+  @media screen and (max-width: 700px) {
+    display: none;
+  }
+`;
+
+export const SOpenLinksButton = styled.button`
+  height: 50px;
+  background: none;
+  border: none;
+  color: black;
+  font-size: 40px;
+  cursor: pointer;
+
+  @media (min-width: 700px) {
+    display: none;
+  }
+`;
+
+export const SNavbarExtendedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  width: 200px;
+  box-shadow: 0px 2px 10px rgb(0 0 0 / 10%);
+
+  @media (min-width: 700px) {
+    display: none;
+  }
+`;
+
+export const SNavbarLinkExtended = styled(Link)`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: none;
+  margin: 10px;
+`;

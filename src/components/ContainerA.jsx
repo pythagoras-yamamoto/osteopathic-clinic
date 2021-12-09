850c2d4d1fd800f7821e51fe48042eef15024c47
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-12-09 15:00:58 +0900

    contianrt margin 改善e

--- src/components/ContainerA.jsx
+++ src/components/ContainerA.jsx
@@ -33,12 +33,15 @@ export const ContainerA = (props) => {
 };
 
 export const SContainerWrapper = styled.div`
-  margin: 50px;
+  margin: auto;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  width: 90vw;
   padding: 50px 0;
   box-shadow: 0px 2px 10px rgb(0 0 0 / 10%);
 
   @media screen and (max-width: 768px) {
-    margin: 50px;
+    margin-top: 50px;
     padding: 50px 0;
   }
 `;
@@ -75,6 +78,7 @@ const SContentsWrapeer = styled.div`
   margin: auto;
   margin-top: 80px;
   width: 80vw;
+  margin-right: 5%;
 
   @media screen and (max-width: 768px) {
     margin-top: 50px;
@@ -87,7 +91,7 @@ const SText = styled.div`
   overflow-wrap: break-word;
   white-space: pre-wrap;
 
-  width: ${(props) => (props.width ? 90 - props.width : 50)}%;
+  width: ${(props) => (props.width ? 95 - props.width : 55)}%;
 
   @media screen and (max-width: 768px) {
     /* font-size: 18px; */

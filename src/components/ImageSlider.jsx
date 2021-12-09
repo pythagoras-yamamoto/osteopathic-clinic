cbc57c0ea0035bda1b92d34a5fa6f585560bf88d
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-12-10 00:05:58 +0900

    swiper 復活

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,91 +1,91 @@
 // import React, { useRef, useState } from "react";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
-// import "swiper/css/effect-coverflow";
-// import "swiper/css/pagination";
-// import SwiperCore, {
-//   Autoplay,
-//   EffectCoverflow,
-//   Pagination,
-//   Mousewheel
-// } from "swiper";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import SwiperCore, {
+  Autoplay,
+  EffectCoverflow,
+  Pagination,
+  Mousewheel
+} from "swiper";
 
-// import Slider from "react-slick";
-// import styled from "styled-components";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-// import Img from "../public/clinic-image.jpeg";
+import Img from "../public/clinic-image.jpeg";
 
-// export const ImageSlider = () => {
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     speed: 1000,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 3000
-//   };
+export const ImageSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  };
 
-//   return (
-//     <ImgWrapper>
-//       <Slider {...settings}>
-//         <SImage src={Img} alt="" />
-//         <SImage src={Img} alt="" />
-//         <SImage src={Img} alt="" />
-//         <SImage src={Img} alt="" />
-//         <SImage src={Img} alt="" />
-//         <SImage src={Img} alt="" />
-//         <SImage src={Img} alt="" />
-//       </Slider>
-//     </ImgWrapper>
-//   );
-// };
+  return (
+    <ImgWrapper>
+      <Slider {...settings}>
+        <SImage src={Img} alt="" />
+        <SImage src={Img} alt="" />
+        <SImage src={Img} alt="" />
+        <SImage src={Img} alt="" />
+        <SImage src={Img} alt="" />
+        <SImage src={Img} alt="" />
+        <SImage src={Img} alt="" />
+      </Slider>
+    </ImgWrapper>
+  );
+};
 
-// const SImage = styled.img`
-//   width: 90vw;
-//   height: auto;
-//   box-shadow: 0px 5px 0 rgb(0 0 0 / 15%);
-// `;
+const SImage = styled.img`
+  width: 90vw;
+  height: auto;
+  box-shadow: 0px 5px 0 rgb(0 0 0 / 15%);
+`;
 
-// // slickの仕様で右側に余白ができる。それを解消するために使用したスタイリング。
-// const ImgWrapper = styled.div`
-//   overflow: hidden;
-// `;
+// slickの仕様で右側に余白ができる。それを解消するために使用したスタイリング。
+const ImgWrapper = styled.div`
+  overflow: hidden;
+`;
 
-// // install Swiper modules
-// SwiperCore.use([EffectCoverflow, Pagination, Autoplay, Mousewheel]);
+// install Swiper modules
+SwiperCore.use([EffectCoverflow, Pagination, Autoplay, Mousewheel]);
 
-// export const SwiperImageSlider = () => {
-//   return (
-//     <ImgWrapper>
-//       <Swiper
-//         spaceBetween={30}
-//         centeredSlides={true}
-//         autoplay={{
-//           delay: 5000,
-//           disableOnInteraction: false
-//         }}
-//         pagination={{
-//           clickable: true
-//         }}
-//         navigation={true}
-//         cssMode={true}
-//         mousewheel={true}
-//         keyboard={true}
-//         className="mySwiper"
-//       >
-//         <SwiperSlide>
-//           <SImage src={Img} alt="" />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <SImage src={Img} alt="" />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <SImage src={Img} alt="" />
-//         </SwiperSlide>
-//       </Swiper>
-//     </ImgWrapper>
-//   );
-// };
+export const SwiperImageSlider = () => {
+  return (
+    <ImgWrapper>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false
+        }}
+        pagination={{
+          clickable: true
+        }}
+        navigation={true}
+        cssMode={true}
+        mousewheel={true}
+        keyboard={true}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <SImage src={Img} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SImage src={Img} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SImage src={Img} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </ImgWrapper>
+  );
+};
